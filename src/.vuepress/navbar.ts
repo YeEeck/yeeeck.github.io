537d75b1b3f209ae409a7d29670c3dcc102c5c8d
yechenkai <yechenkai@wuruitech.com>
2025-02-27 11:07:49 +0800

    主页导航增加硬件栏

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -3,6 +3,14 @@ import { navbar } from "vuepress-theme-hope";
 export default navbar([
   "/",
   {
+    text: "硬件与上位机",
+    icon: "book",
+    prefix: "/posts/hardware/",
+    children: [
+      "SentIntroduce"
+    ],
+  },
+  {
     text: "前端",
     icon: "pen-to-square",
     prefix: "/posts/frontend/",
